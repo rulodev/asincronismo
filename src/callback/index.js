56e20a2f60bd0a894d7fddf1b6945f55a8400641
Raúl González <raulg0304@gmail.com>
2021-02-03 21:58:29 -0600

    Callback example

--- src/callback/index.js
+++ src/callback/index.js
@@ -1,1 +1,22 @@
-console.log("Hola mundo")
+//console.log("Hola mundo")
+
+function imCallbacl(data)
+{
+    console.log("Im the callback, is my turn")
+    console.log(data)
+}
+
+function httpRequest(callbackTest)
+{
+    console.log("Im doing some operations.")
+    console.log("Finishing")
+    callbackTest(
+        {
+            "id": 21,
+            "name": "Raúl González",
+            "age": 28
+        }
+    )
+}
+
+httpRequest(imCallbacl)
